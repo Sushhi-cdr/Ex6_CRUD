27779f6220539e2353c385092f16ee74bc53f0f3
unknown <enzoseiti@email.com>
2026-03-17 19:41:01 -0300

    Criação do Services

--- src/main/java/com/example/Ex6/Repositories/repositorioClientes.java
+++ src/main/java/com/example/Ex6/Repositories/repositorioClientes.java
@@ -3,5 +3,5 @@ package com.example.Ex6.Repositories;
 import com.example.Ex6.Models.Clientes;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface repositorioClientes extends JpaRepository<Clientes, Long> {
+public interface RepositorioClientes extends JpaRepository<Clientes, Long> {
 }
---
+++ src/main/java/com/example/Ex6/Services/ServicoClientes.java
@@ -0,0 +1,31 @@
+package com.example.Ex6.Services;
+
+import com.example.Ex6.Models.Clientes;
+import com.example.Ex6.Repositories.RepositorioClientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ServicoClientes {
+
+    @Autowired
+    private RepositorioClientes repositorio;
+
+    public Clientes criar(Clientes clientes) {
+        return repositorio.save(clientes);
+    }
+
+    public List<Clientes> listar(){
+        return repositorio.findAll();
+    }
+
+    public Clientes buscar(Long id){
+        return repositorio.findById(id).orElse(null);
+    }
+
+    public void remover(Long id){
+        repositorio.deleteById(id);
+    }
+}
